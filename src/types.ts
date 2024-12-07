e78bab01015514655a5316143c4373a5df57531e
vitthal-07 <vitthalbiradar50@gmail.com>
2024-12-07 11:36:58 +0530

    Fixed some bugs

--- src/types.ts
+++ src/types.ts
@@ -5,15 +5,15 @@ export interface Product {
   description: string;
   price: number;
   image: ProductImage[];
-  category: "Women" | "Men" | "Kids";
-  subCategory: "Topwear";
+  category: string;
+  subCategory: string;
   sizes: ("S" | "M" | "L" | "XL" | "XXL")[];
   date: number;
   bestseller: boolean;
 }
 
-export interface Cart  {
+export interface Cart {
   [itemId: string]: {
     [size: string]: number;
   };
-};
+}
